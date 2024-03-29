981310dbb3dbfd0b78548501ddc3dd1ce1be8e5d
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-03-29 18:24:43 +0100

    commit

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,8 +1,35 @@
 import { Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import HomePageFetch from "./HomePageFetch";
+import { useSelector } from "react-redux";
 
 const MainSection = () => {
+  let array = [
+    {
+      name: "queen",
+      sectionName: "Rock Classic",
+    },
+    {
+      name: "katyperry",
+      sectionName: "Pop Classic",
+    },
+    {
+      name: "eminem",
+      sectionName: "Rap",
+    },
+  ];
+  const selector = useSelector((state) => {
+    if (state !== "") {
+      array = [
+        {
+          name: state.music.search,
+          sectionName: "Search Results",
+        },
+      ];
+    }
+    return state.music.search;
+  });
+
   return (
     <Col lg={12} md={{ span: 9, offset: 2 }} className="mainPage">
       <Row>
@@ -22,43 +49,23 @@ const MainSection = () => {
           </div>
         </Col>
       </Row>
-      <Row>
-        <Col lg={10}>
-          <div id="rock">
-            <h2>Rock Classics</h2>
-            <Row className=" py-3" id="rockSection">
-              <div className="d-flex">
-                <HomePageFetch artistName="queen" />
-              </div>
-            </Row>
-          </div>
-        </Col>
-      </Row>
 
-      <Row>
-        <Col lg={10}>
-          <div id="pop">
-            <h2>Pop Classics</h2>
-            <Row className="py-3" id="popSection">
-              <div className="d-flex">
-                <HomePageFetch artistName="katyperry" />
+      {array.map((e, i) => {
+        return (
+          <Row>
+            <Col lg={10}>
+              <div id={i} className="rock">
+                <h2>{e.sectionName}</h2>
+                <Row className=" py-3" id="rockSection">
+                  <div className="d-flex">
+                    <HomePageFetch artistName={e.name} />
+                  </div>
+                </Row>
               </div>
-            </Row>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={10}>
-          <div id="hiphop">
-            <h2>HipHop</h2>
-            <Row className="py-3" id="hipHopSection">
-              <div className="d-flex">
-                <HomePageFetch artistName="eminem" />
-              </div>
-            </Row>
-          </div>
-        </Col>
-      </Row>
+            </Col>
+          </Row>
+        );
+      })}
     </Col>
   );
 };
